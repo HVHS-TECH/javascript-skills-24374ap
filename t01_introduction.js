06375ed0f10af5f7f9e9ad4ee2aa479333f6db95
24374ap <24374ap@hvhs.school.nz>
2026-04-27 21:48:59 +0000

    Apr 27, 2026, 9:48 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,9 @@
 /****************************
 Name of task:1
 ****************************/
-
 console.log("Running t01_introduction.js")
+
 console.log(/*Insert your message here!*/)
 
-// This is a single line comment
+// vari
 

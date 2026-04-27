cf694b5d7a4dd34a23476f6f06a6a8b5d3b5cc42
24374ap <24374ap@hvhs.school.nz>
2026-04-27 21:46:38 +0000

    Apr 27, 2026, 9:46 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,6 +1,6 @@
 /****************************
-This is a block comment.
-Put your header comment here!
+Name of task:1
+
 ****************************/
 console.log("Running t01_introduction.js")
 console.log(/*Insert your message here!*/)

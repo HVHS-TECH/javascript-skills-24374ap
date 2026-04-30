066b25845460ea273f006e56ccae9375a4f71dcc
24374ap <24374ap@hvhs.school.nz>
2026-04-30 01:40:20 +0000

    Apr 30, 2026, 1:40 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,2 +1,9 @@
-var name="adhil";
-console.log("name is " + name);
+/****************************
+Name of task:2
+****************************/
+console.log("Running t02_introduction.js")
+console.log('adhil')
+
+// variables
+
+

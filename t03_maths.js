e5cb414938ceb564745d82e94d2b034fa1b31247
24374ap <24374ap@hvhs.school.nz>
2026-05-04 21:31:39 +0000

    May 4, 2026, 9:31 PM

--- t03_maths.js
+++ t03_maths.js
@@ -2,7 +2,7 @@ var userName ='adhil';
 var youareage =15;
 var pocktemoney=2000;
 var currentyear=11;
-var youare
+var asof
 //MAIN CODE
 console.log('hi ' + userName)
 console.log('you are' + youareage)

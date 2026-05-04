b5884d920132f00d04dcca7525edee4af2d7e160
24374ap <24374ap@hvhs.school.nz>
2026-05-04 21:55:48 +0000

    May 4, 2026, 9:55 PM

--- t03_maths.js
+++ t03_maths.js
@@ -2,9 +2,11 @@ var userName ='adhil';
 var youareage =15;
 var pocktemoney=2000;
 var currentyear=2026;
-var Youwerebornin=2010
+var Youwerebornin=2010;
+var In10yearsyouwillbeyearsold=25;
 console.log('hi ' + userName)
 console.log('you are' + youareage)
 console.log('pockte money' +pocktemoney)
 console.log('As of '+currentyear + " you are "+youareage )
 console.log('You were born in '+Youwerebornin)
+console.log('In 10 years you will be years old '+In10yearsyouwillbeyearsold)

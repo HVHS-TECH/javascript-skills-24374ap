f2516b8bfa78b850932846ec5f76e1923b8724f6
24374ap <24374ap@hvhs.school.nz>
2026-05-07 00:56:33 +0000

    May 7, 2026, 12:56 AM

--- t03_maths.js
+++ t03_maths.js
@@ -5,7 +5,7 @@ var currentyear=2026;
 var Youwerebornin=2010;
 var In10yearsyouwillbeyearsold=25;
 console.log('hi ' + userName)
-console.log('you are' + youareage)
+console.log('you are ' + youareage)
 console.log('pockte money' +pocktemoney)
 console.log('As of '+currentyear + " you are "+youareage )
 console.log('You were born in '+Youwerebornin)

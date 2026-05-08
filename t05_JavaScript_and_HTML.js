4343a24ee19622066fa890653e0f69be3a4b4ad4
24374ap <24374ap@hvhs.school.nz>
2026-05-08 02:38:16 +0000

    May 8, 2026, 2:38 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,12 @@
-/* Paste your code from task 3, Maths here */
+var userName ='adhil';
+var youareage =15;
+var pocktemoney=2000;
+var currentyear=2026;
+var Youwerebornin=2010;
+var In10yearsyouwillbeyearsold=25;
+console.log('hi ' + userName)
+console.log('you are ' + youareage)
+console.log('pockte money' +pocktemoney)
+console.log('As of '+currentyear + " you are "+youareage )
+console.log('You were born in '+Youwerebornin)
+console.log('In 10 years you will be years old '+In10yearsyouwillbeyearsold)

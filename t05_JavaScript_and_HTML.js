f902553e179ee79fca6c5f8fdb252df7bd5d467f
24374ap <24374ap@hvhs.school.nz>
2026-05-08 02:57:03 +0000

    May 8, 2026, 2:57 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -6,7 +6,7 @@ var Youwerebornin=2010;
 var In10yearsyouwillbeyearsold=25;
 console.log('hi ' + userName)
 console.log('you are ' + youareage)
-console.log('pockte money' +pocktemoney)
+console.log('pockte money ' +pocktemoney)
 console.log('As of '+currentyear + " you are "+youareage )
 console.log('You were born in '+Youwerebornin)
 console.log('In 10 years you will be years old '+In10yearsyouwillbeyearsold)

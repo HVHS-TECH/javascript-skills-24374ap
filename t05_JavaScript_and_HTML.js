f01194682ab0677fda18a40753993cc321547b2a
24374ap <24374ap@hvhs.school.nz>
2026-05-14 01:19:32 +0000

    May 14, 2026, 1:19 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,2 +1,2 @@
-const OUTPUT = document.getElementById("JavaScriptOutput");
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2></h2>";

5ed52f305b8951a9d3565b304a9baf94ed4e7658
24374ap <24374ap@hvhs.school.nz>
2026-05-18 21:43:52 +0000

    May 18, 2026, 9:43 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,10 @@
 /* Paste your code from task 6 here */
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>hi ryan</h2>";
+/* Paste your code from task 5 here */
+
+function writeline(){
+    // Add aline to the html page
+    OUTPUT.innerHTML += "<h2>hi ryan</h2>";
+}
+writeline()

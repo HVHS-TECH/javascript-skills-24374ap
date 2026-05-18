ce6fb9a2bf4a6e649513d68215adf32935faf3f2
24374ap <24374ap@hvhs.school.nz>
2026-05-18 21:41:19 +0000

    May 18, 2026, 9:41 PM

--- t06_functions.js
+++ t06_functions.js
@@ -2,4 +2,7 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML = "<h2>hi ryan</h2>";
 /* Paste your code from task 5 here */
 
-function writeline(){}
+function writeline(){
+    // Add aline to the html page
+    OUTPUT.innerHTML += "<h2>hi ryan</h2>";
+}

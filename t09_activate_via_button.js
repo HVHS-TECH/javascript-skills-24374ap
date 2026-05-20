4899ad6f97e013836fc9f89ca2693d0ae85058fd
24374ap <24374ap@hvhs.school.nz>
2026-05-20 00:15:53 +0000

    May 20, 2026, 12:15 AM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,17 @@
-/* Paste your code from task 7 here*/
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>hi ryan</h2>";
+/* Paste your code from task 5 here */
+
+function writeline(_name){
+    // Add aline to the html page
+    OUTPUT.innerHTML += "<p>welcome to the page"+_name+" </p>";
+}
+writeline("Mr Bob");
+writeline("Ms alice ");
+function displaywelcome(_name,_age){
+    OUTPUT.innerHTML +="<p>welcome to the page"+_name+"</p>";
+    OUTPUT.innerHTML +="<p>you are"+_age+" years old</p>";
+}
+displaywelcome("Ms Alice", 16);
+displaywelcome("Mr Bob", 99);

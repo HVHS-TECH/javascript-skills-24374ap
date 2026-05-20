810ef90727416d88f96259613c9d481eae1e736d
24374ap <24374ap@hvhs.school.nz>
2026-05-20 00:14:46 +0000

    May 20, 2026, 12:14 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -13,5 +13,5 @@ function displaywelcome(_name,_age){
     OUTPUT.innerHTML +="<p>welcome to the page"+_name+"</p>";
     OUTPUT.innerHTML +="<p>you are"+_age+" years old</p>";
 }
-displayWelcome("Ms Alice", 16);
-displayWelcome("Mr Bob", 99);
+displaywelcome("Ms Alice", 16);
+displaywelcome("Mr Bob", 99);

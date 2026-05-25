c18a73cee20d9dd9a29bc4f21a2303b67d8d7079
24374ap <24374ap@hvhs.school.nz>
2026-05-25 21:45:53 +0000

    May 25, 2026, 9:45 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,21 @@
 /* Paste your code from the last task */
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>hi ryan</h2>";
+/* Paste your code from task 5 here */
+
+function writeline(_name){
+    // Add aline to the html page
+    OUTPUT.innerHTML += "<p>welcome to the page"+_name+" </p>";
+}
+function displaywelcome(_name,_age){
+    OUTPUT.innerHTML +="<p>welcome to the page"+_name+"</p>";
+    OUTPUT.innerHTML +="<p>you are"+_age+" years old</p>";
+}
+
+function start() {
+    writeline("Mr Bob");
+    writeline("Ms alice ");
+    displaywelcome("Ms Alice", 16);
+    displaywelcome("Mr Bob", 99);
+}
+

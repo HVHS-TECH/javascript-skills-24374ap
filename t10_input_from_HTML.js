cb16704d9cc5c20549dfe46eb9e0733988defd93
24374ap <24374ap@hvhs.school.nz>
2026-05-28 01:17:46 +0000

    May 28, 2026, 1:17 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,28 +1,5 @@
-/* Paste your code from the last task */
+/* Paste your code from the last task 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML = "<h2>hi ryan</h2>";
 /* Paste your code from task 5 here */
-
-function writeline(_name){
-    // Add aline to the html page
-    OUTPUT.innerHTML += "<p>welcome to the page"+_name+" </p>";
-}
-function displaywelcome(_name,_age){
-    OUTPUT.innerHTML +="<p>welcome to the page"+_name+"</p>";
-    OUTPUT.innerHTML +="<p>you are"+_age+" years old</p>";
-}
-
-function start() {
-    writeline("Mr Bob");
-    writeline("Ms alice ");
-    displaywelcome("Ms Alice", 16);
-    displaywelcome("Mr Bob", 99);
-}
-const NAME_FIELD = document.getElementById("nameField");
-let userName = NAME_FIELD.value;
-function getFormInput(){
-  const NAME_FIELD = document.getElementById("nameField");
-  let userName = NAME_FIELD.value;
-  OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
-}
-
+Iet username;

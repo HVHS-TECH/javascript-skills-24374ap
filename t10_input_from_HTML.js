10ff2cdff2f44af07d7c7868b3fa5d8d716fb73f
24374ap <24374ap@hvhs.school.nz>
2026-05-28 01:26:14 +0000

    May 28, 2026, 1:26 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,11 +1,11 @@
 /* Paste your code from the last task 
-let username;
+let userName;
 let userName = 15;
 let pocketmoney = 5;
 let currentyear =2026
 
 
-function getFormInput(){
+function getuserName(){
   const NAME_FIELD = document.getElementById("nameField");
   let userName = NAME_FIELD.value;
   OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";

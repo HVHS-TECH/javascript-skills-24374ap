7fadb3de3a9c0e26441a2650ada7de6e9b2d377a
24374ap <24374ap@hvhs.school.nz>
2026-05-28 01:19:35 +0000

    May 28, 2026, 1:19 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,5 +1,3 @@
 /* Paste your code from the last task 
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML = "<h2>hi ryan</h2>";
-/* Paste your code from task 5 here */
-Iet username;
+let username;
+let u
